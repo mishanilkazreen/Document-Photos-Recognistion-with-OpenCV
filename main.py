b0a3229e0298491c90e55d281e6a3f2519db7e01
mishanil <mishanilkazreen@gmail.com>
2025-10-03 17:07:59 +0100

    Removed Contour Detection to grab the corners of the document.
    Utilising Hough Lines algorithm instead.

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 # Libraries
-import sys
-import os
 from glob import glob
 
 import cv2
-
-import math
 
 import matplotlib
 import matplotlib.pyplot as plt
 
 import numpy as np
-from fontTools.misc.bezierTools import epsilon
 from numpy import ndarray
-
 
 # Function: Converting image files into matrices to operate and calculate on.
 def convert_img_to_data(file_name: str) -> ndarray:
-    file_data = cv2.imread(file_name, 1) # Reading as BGR colour data
+    file_data = cv2.imread(file_name, cv2.IMREAD_COLOR) # Reading as BGR colour data
 
     return file_data
 
@@ -48,35 +42,86 @@ def apply_canny_on_img(file_data: ndarray) -> ndarray:
 
     return img_canny
 
+# Function: Conducts a matrix multiplication to find the intersection points between two lines
+# Goal (x,y) from: p = x*sin(theta) + y*sin(theta)
+def find_intersection(line_1: ndarray, line_2: ndarray) -> ndarray:
+    rho_1, theta_1 = line_1
+    rho_2, theta_2 = line_2
+
+    # cos(theta) sin(theta)
+    matrix_a = np.array([
+        [np.cos(theta_1), np.sin(theta_1)],
+        [np.cos(theta_2), np.sin(theta_2)]
+    ])
+
+    # p
+    matrix_b = np.array([[rho_1], [rho_2]])
+
+    # Solving simultaneous equations to obtain the intersection points
+    try:
+        x0, y0 = np.linalg.solve(matrix_a, matrix_b)
+
+        return [int(np.round(x0).item()), int(np.round(y0).item())]
+
+    except np.linalg.LinAlgError:
+        return None
+
+# Function: Processing an image and obtaining line intersections, and grabbing the corners of the object
 def extract_document_corners(file_data: ndarray) -> ndarray:
-    largest_quadrilateral = None
-    max_area = 0
-    
-    contours, _ = cv2.findContours(file_data, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    line_intersections = []
+    hough_lines_params = dict(
+        rho=1,
+        theta=(np.pi / 180), # Radians
+        threshold=180
+    )
 
-    for contour in contours:
-        tolerance = cv2.arcLength(contour, True) * 0.02
-        approximation = cv2.approxPolyDP(contour, tolerance, True)
-        
-        # Checks to see if the 
-        if len(approximation) == 4:
-            area = cv2.contourArea(approximation)
-            
-            if area > max_area:
-                max_area = area
-                largest_quadrilateral = approximation
+    canny_img = apply_canny_on_img(file_data) # Pre-processing for Hough Transform
 
-    if largest_quadrilateral is not None:
+    # https://learnopencv.com/hough-transform-with-opencv-c-python/
+    lines = cv2.HoughLines(canny_img, **hough_lines_params)
 
-        return largest_quadrilateral.reshape(4, 2)
-    else:
+    if lines is None:
+        return None
 
-        return np.array([])
+    lines.reshape(-1, 2) # Convert to a 2D matrix
+
+    for i in range(len(lines)):
+
+        for v in range(i+1, len(lines)):
+            cords = find_intersection(lines[i], lines[v])
+
+            if cords is not None:
+                x, y = cords
+
+                # Check to make sure the coordinates are in the file resolution
+                if 0 <= x < file_data.shape[1] and 0 <= y < file_data.shape[0]:
+                    line_intersections.append(cords)
+
+    line_intersections = np.array(line_intersections) # Convert array -> ndarray
+
+    if len(line_intersections) < 4: # We need at least 4 intersections otherwise we won't be able to formulate a quadrilateral :(
+        return None
+
+    # Heuristical Approach. We will obtain the furthest point intersections based on region
+    sums = line_intersections.sum(axis=1)
+    differences = np.diff(line_intersections, axis=1).flatten()
+
+    top_left = line_intersections[np.argmin(sums)]
+    bottom_right = line_intersections[np.argmax(sums)]
+
+    top_right = line_intersections[np.argmin(differences)]
+    bottom_left = line_intersections[np.argmax(differences)]
+
+    corners = np.array([top_left, top_right, bottom_right, bottom_left])
+
+    return corners
+
 
 ## canny -> detect the strongest edges -> apply homography -> threshold -> run through ocr model -> get text
 
 if __name__ == '__main__':
-    file_to_load = input(f"Enter the file directory to process.")
+    file_to_load = "img/LinedPaperTemplate.png" # input(f"Enter the file(s) directory to process.")
 
     some_variable_name = load_img_into_memory(file_to_load)
+    some_data_name = extract_document_corners(some_variable_name[0])
 
